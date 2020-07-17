792fc71a39573193901f2c83a718c4982cb3bb62
zhouzhouzhouyao <zhou13693908265@163.com>
2020-07-17 20:48:00 +0800

    【add】：新增项目初始化配置文件以及入口启动文件

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const { override, fixBabelImports } = require('customize-cra');
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css'
+    })
+)
+//用于配置antd按需加载
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNav from './components/TopNav'
+
+function App() {
+  return (
+    <div>
+        <TopNav/>
+    </div>
+  );
+}
+
+export default App;
