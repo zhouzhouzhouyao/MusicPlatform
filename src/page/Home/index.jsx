f214243e12e2f9ea29eb9fbd0f0db151f716d780
zhouzhouzhouyao <zhou13693908265@163.com>
2020-07-18 09:39:47 +0800

    【add】：新增首页轮播图

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { component } from "react";
+import { Carousel } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import './style.css'
 
-const Home = () => {
-    return(
-        <div>
-            主页
-        </div>
-    )
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.next = this.next.bind(this);
+        this.prev = this.prev.bind(this);
+    }
+
+    next() {
+        this.slider.slick.slickNext();
+    }
+
+    prev() {
+        this.slider.slick.slickPrev();
+    }
+
+    render() {
+        const lunboSetting = {
+            dots: true,
+            autoplay:true,
+        }
+        return(
+            <div>
+                <LeftOutlined className="left-out-line"  onClick={ this.prev }/>
+                <Carousel {...lunboSetting} ref={el => (this.slider = el)}>
+                    <div>
+                        <h3>1</h3>
+                    </div>
+                    <div>
+                        <h3>2</h3>
+                    </div>
+                    <div>
+                        <h3>3</h3>
+                    </div>
+                    <div>
+                        <h3>4</h3>
+                    </div>
+                </Carousel>
+                <RightOutlined className="right-out-line" onClick={ this.next }/>
+            </div>
+        )
+    }
 }
+
 
 export default Home
